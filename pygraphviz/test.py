b2256950b890bb3b07aeea242290715a04cf1bbb
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-06-08 17:39:31 -0300

    agregando ejemplo de interpolacion

--- pygraphviz/test.py
+++ pygraphviz/test.py
@@ -1,9 +1,12 @@
 import pygraphviz as gv
 
 g = gv.AGraph(directed=True)
-#nombre = g.add_node("nombre")
+nombre = g.add_node("nombre")
+nombre = g.add_node("WebKit")
 #pepe = g.add_node("pepe")
-g.add_edge("nombre", "pepe")
+g.add_edge("WebKit", "index")
+g.add_edge("WebKit", "broken")
+g.add_edge("nombre", "broken")
 
-g.draw("test.png", prog='dot')
+g.draw("test.dot", prog='dot')
 
